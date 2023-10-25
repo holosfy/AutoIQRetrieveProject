489953c303311afcb690a4e762576989ff97cc29
holosfy <3320681251@qq.com>
2023-10-25 23:43:24 +0800

    add qianwen in content learn

--- test/qwen_14_icl_test.py
+++ test/qwen_14_icl_test.py
@@ -34,7 +34,7 @@ def get_submit_data():
     res = []
     prompt_template = """
        现在你是一个吉利旗下的领克品牌的汽车专家，来负责解答下面与汽车驾驶操作相关的问题。
-       你可以需要参考下面汽车相关的知识给出答案。下面是汽车知识的内容：
+       你可以参考下面汽车相关的知识给出答案。下面是汽车知识的内容：
        ''' {in_content} '''
        
        通过学习上面的汽车知识给出简要的概括性的答案，最好在100个字以内。遇到不知道的问题，诚实的回答不知道。下面是问题：
@@ -52,21 +52,18 @@ def get_submit_data():
 
         prompt = prompt_template.format(in_content=in_content, query=question)
 
-
         print(prompt)
         print("answer_1")
         qa['answer_1'] = get_chat_result(prompt)
         print("answer_2")
-        qa['answer_2'] = "" # get_chat_result(prompt)
+        qa['answer_2'] = get_chat_result(prompt)
         print("answer_3")
-        qa['answer_3'] = "" # get_chat_result(prompt)
+        qa['answer_3'] = get_chat_result(prompt)
         res.append(qa)
 
     filename = model + ".json"
     with open(filename, 'w', encoding='utf-8') as file_obj:
         json.dump(res, file_obj, ensure_ascii=False)
-
-
 
 
 if __name__ == '__main__':
